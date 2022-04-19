a58d5fdd2cbf9ec982d86e04c47dbc7464008af8
MorenaNobre <morenagnobre@gmail.com>
2022-04-19 13:40:50 -0300

    First attempt to consume NBA api - failed - Unauthorized - see how to use the access key

---
+++ src/App.tsx
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react";
+
+type Team = {
+  name: string;
+  nickname: string;
+  logo: string;
+};
+
+function App() {
+  const [teams, setTeams] = useState<Team[]>([]);
+
+  useEffect(() => {
+    fetch("https://api-nba-v1.p.rapidapi.com/teams")
+      .then((response) => response.json())
+      .then((data) => {
+        setTeams(data);
+      });
+  }, []);
+
+  return (
+    <>
+      <h1>mother fucker</h1>
+      <ul>
+        {teams.map((team) => {
+          return (
+            <li key={team.name}>
+              <img src={team.logo} alt="team logo" />
+              <strong>{team.name}</strong>
+              <p>{team.nickname}</p>
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
+}
+
+export default App;

0d73e10390b04d2891e64b38dcd7e4298b9dc2e3
MorenaNobre <morenagnobre@gmail.com>
2022-04-20 19:54:37 -0300

    Trying to use axios for the api

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,42 @@
-import { useEffect, useState } from "react";
+import axios from "axios"
+import api from "./services/api"
+// import { useAPI } from "./hooks/useAPI";
 
-type Team = {
-  name: string;
-  nickname: string;
-  logo: string;
-};
+// type Team = {
+//   name: string;
+//   nickname: string;
+//   logo: string;
+// };
 
 function App() {
-  const [teams, setTeams] = useState<Team[]>([]);
+  // const { data } = useAPI<Team[]>("https://api-basketball.p.rapidapi.com/teams")
+  // const { data } = useAPI<Team[]>("options")
 
-  // useEffect(() => {
-  //   fetch("https://api-nba-v1.p.rapidapi.com/teams")
-  //     .then((response) => response.json())
-  //     .then((data) => {
-  //       setTeams(data);
-  //     });
-  // }, []);
+  const axios = require("axios");
+
+  const options = {
+    method: "GET",
+    url: "https://api-basketball.p.rapidapi.com/teams",
+    headers: {
+      "X-RapidAPI-Host": "api-basketball.p.rapidapi.com",
+      "X-RapidAPI-Key": "eb38d95969mshf802fa7076ec9abp19bbe5jsnb91973967047",
+    },
+  };
+
+  axios
+    .request(options)
+    .then(function (response:Response) {
+      console.log(options);
+    })
+    .catch(function (error:Error) {
+      console.error(error);
+    });
 
   return (
     <>
       <h1>Hello World!</h1>
       {/* <ul>
-        {teams.map((team) => {
+        {data?.map((team) => {
           return (
             <li key={team.name}>
               <img src={team.logo} alt="team logo" />

a9df04c2df5e3584be45c8e76199938e447d6d51
MorenaNobre <morenagnobre@gmail.com>
2022-04-22 18:01:12 -0300

    Got api working - trying to find out how to bring all the teams to a list by their id

--- src/App.tsx
+++ src/App.tsx
@@ -14,24 +14,21 @@ function App() {
       method: "GET",
       headers: {
         "X-RapidAPI-Host": "api-basketball.p.rapidapi.com",
-        "X-RapidAPI-Key": "XxXxXxXxXxX",
+        "X-RapidAPI-Key": "eb38d95969mshf802fa7076ec9abp19bbe5jsnb91973967047",
       },
     };
 
-    fetch(
-      "https://api-nba-v1.p.rapidapi.com/teams",
-      options
-    )
+    fetch("https://api-nba-v1.p.rapidapi.com/teams?id=1", options)
       .then((response) => response.json())
-      .then(data => {
-        setTeams(data)
+      .then((data) => {
+        setTeams(data.response);
       })
       .catch((err) => console.error(err));
   }, []);
-  
+
   return (
     <ul>
-      {teams.map(team => {
+      {teams.map((team) => {
         return (
           <li key={team.name}>
             <img src={team.logo} alt="Team logo" />
